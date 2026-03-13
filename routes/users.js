99b442f1acaf22e5bc7e0a6f9fc08ab836aad12b
Jace Deng <jacedeng@icloud.com>
2026-03-13 15:07:23 +0800

    vup y/

--- routes/users.js
+++ routes/users.js
@@ -6,5 +6,6 @@ const userController = require("../controllers/userController")
 router.post("/login", userController.login)
 router.get("/", userController.getUsers)
 router.delete("/:id", userController.deleteUser)
+router.post("/:id/reset", userController.resetUser)
 
 module.exports = router
